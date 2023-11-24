989d0dd89f9111253af9df9f4549fa35fc8214d8
Yesigomwe <yesigek@gmail.com>
2023-11-24 19:04:50 +0300

    Configure the project to use Mantine component library.

--- NutriLife/src/Home.jsx
+++ NutriLife/src/Home.jsx
@@ -1,6 +1,8 @@
 import './Home.css';
 import {useState} from 'react';
 import NavigationBar from './components/Header';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 function Home() {
 
   const [message, updateMassage] = useState("G5")
@@ -11,14 +13,18 @@ function Home() {
   }
 
   return (
-
-    <home>
+    <MantineProvider>
+<home>
         <NavigationBar/>
       <div>
         <h2>Welcome to {message}! </h2>
-        <button onClick={handleClick}>Click</button>
+        <button className='bg-[#FE6314] rounded-lg width: 30px' onClick={handleClick}>Click</button>
       </div>
     </home>
+
+    </MantineProvider>
+
+    
   )
 }
 
