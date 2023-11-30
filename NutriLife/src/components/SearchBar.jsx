da7039255311706ca8b2ff6f3e1070eb99e15953
Yesigomwe <yesigek@gmail.com>
2023-11-30 11:36:42 +0300

    Address the feedback raised

--- NutriLife/src/components/SearchBar.jsx
+++ NutriLife/src/components/SearchBar.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import {useState} from "react"
 import { MantineProvider} from "@mantine/core";
 
 const SearchBar = () => {
 
-	const [searchInput, setSearchInput] = useState("Search here")
+	/*
+	Aaron great job so far but the functionality is incomplete.
+
+	Initiate another usestate function that will initiate the search. So far you are only getting the input from the user but not doing anything with that user input. 
+	Add the search icon to this bar as appears in the figma design.
+
+	I have changed the border color too so that the field appears as is.
+
+	*/
+	const [searchInput, setSearchInput] = useState('') //Initial value is empty because initially there is no search text, you just need a place holder.
 
 	const handleChange = (e) => {
 		e.preventDefault();
@@ -14,8 +24,8 @@ const SearchBar = () => {
        
        <MantineProvider>
 		<div>
-		<i class="fa fa-search" aria-hidden="true"></i>
-	<input className="bg-orange-300 rounded-full px-40 my-8 py-3" type="text"
+		<i className="fa fa-search" aria-hidden="true"></i>
+	<input className="bg-white border-solid border-2 border-[#FE6314] rounded-full px-40 my-8 py-3" type="text"
 			 placeholder="search here"  onChange ={handleChange}
 				value={searchInput} />		
 				
