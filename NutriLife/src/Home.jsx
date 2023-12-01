41d6c8748c77efb6d015b9ea28f81e89ef17fb4e
Yesigomwe <yesigek@gmail.com>
2023-12-01 11:14:52 +0300

    Reset Home.jsx to remove conflict but all is good

--- NutriLife/src/Home.jsx
+++ NutriLife/src/Home.jsx
@@ -1,36 +1,12 @@
-import './Home.css';
-import {useState} from 'react';
-import NavigationBar from './components/Header';
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import SearchBar from './components/SearchBar';
-import Footer from './components/Footer';
+import "./Home.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 function Home() {
-
-  const [message, updateMassage] = useState("G5")
-
-  const handleClick = () => {
-    updateMassage("Nutrilife");
-
-  }
-
   return (
     <MantineProvider>
-<home>
-  
-        <NavigationBar/>
-        
-      <div>
-        <h2>Welcome to {message}! </h2>
-        <button className='bg-[#FE6314] rounded-lg width: 30px' onClick={handleClick}>Click</button>
-        
-        <SearchBar/>
-      </div>
-      <Footer/>
-    </home>
-
+      <home></home>
     </MantineProvider>
-  )
+  );
 }
 
-export default Home
+export default Home;
