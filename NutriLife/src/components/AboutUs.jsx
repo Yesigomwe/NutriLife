fd42ac08aaad46501062061ed842345309e30641
Yesigomwe <yesigek@gmail.com>
2023-12-09 12:39:58 +0300

    Create the about page and add data

--- NutriLife/src/components/AboutUs.jsx
+++ NutriLife/src/components/AboutUs.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+
 export function AboutUs() {
+  const apiUrl =
+    "http://localhost:1337/api/team-members?populate[display_image][populate]=*";
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result.data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
   return (
     <div className=" flex flex-col items-center">
       <div className=" about-text bg-green w-1290 h-700">
@@ -6,7 +31,7 @@ export function AboutUs() {
         <div className="text-30 text-white">
           <p>
             Are you looking for a recipe to make a healthy meal? <br />
-            You are in the right place. We have recipes for all your needs.{" "}
+            You are in the right place. We have recipes for all your needs.
           </p>
           <br />
           <p>
@@ -24,46 +49,40 @@ export function AboutUs() {
       <h1 className="border-2 border-solid border-orange text-orange font-bold text-48 w-1290 mt-8 mb-8">
         Meet Our <span className="text-green">Team</span>
       </h1>
-      <div>
-        <div className="bg-green w-1290 m-4 p-4 grid gap-64 grid-cols-2 grid-rows-3">
-          <div className="w-300 h-300">
-            <Profile />
-          </div>
-          <div className="w-300 h-300">
-            <Profile />
-          </div>
-          <div className="w-300 h-300">
-            <Profile />
-          </div>
-          <div className="w-300 h-300">
-            <Profile />
-          </div>
-          <div className="w-300 h-300">
-            <Profile />
-          </div>
-        </div>
+
+      <div className="teamMember bg-green w-1290 m-4 p-4 h-auto flex flex-wrap justify-center mt-8 mb-8">
+        {Array.isArray(data) ? (
+          data.map((teamMember) => (
+            <div
+              className="m-8 w-96 flex flex-col m-2 items-center justify-center"
+              key={teamMember.id}
+            >
+              {teamMember.attributes.display_image && (
+                <img
+                  src={`http://localhost:1337${teamMember.attributes.display_image[0].image.data.attributes.formats.thumbnail.url}`}
+                  alt="image"
+                  className="h-300 w-300 rounded-5"
+                />
+              )}
+              <div className="info flex w-300 flex-col items-start bg-orange text-green font-bold">
+                <p>{teamMember.attributes.name}</p>
+                <p>
+                  <a
+                    target="_blank"
+                    href={teamMember.attributes.website}
+                    rel="noreferrer"
+                  >
+                    {teamMember.attributes.name}.com
+                  </a>
+                </p>
+                <p>{teamMember.attributes.bio}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
-  );
-}
-
-import { Card, Image, Text, Group } from "@mantine/core";
-
-function Profile() {
-  return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image src="src/assets/kennedy.jpeg" height={160} alt="profile_image" />
-      </Card.Section>
-
-      <Group className="flex flex-col" justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Kennedy</Text>
-        <Text fw={500}><a href="#">@Kennedy</a></Text>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        This person contributed to the building of this project.
-      </Text>
-    </Card>
   );
 }
