da772a74fb01e6a878579751352a8463646f5f04
Kennedy <46450549+Yesigomwe@users.noreply.github.com>
2023-12-11 12:43:31 +0300

    Delete Backend directory
    I am deleting this directory to keep the frontend repo lighter. I have created a different repo for the backend

--- Backend/src/api/recipe-detail/services/recipe-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recipe-detail service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::recipe-detail.recipe-detail');
