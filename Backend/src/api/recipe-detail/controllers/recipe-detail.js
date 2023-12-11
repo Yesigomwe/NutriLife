8784058e8f91129b97680c9d92fa9de8ea00754d
Kennedy <46450549+Yesigomwe@users.noreply.github.com>
2023-12-11 12:45:21 +0300

    Delete Backend directory

--- Backend/src/api/recipe-detail/controllers/recipe-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recipe-detail controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::recipe-detail.recipe-detail');
